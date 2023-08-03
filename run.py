d334dac275a59a0f8b58b41da91f0482bfd41e78
SeanHub2021 <shbristol12@gmail.com>
2023-08-03 22:02:14 +0000

    improve commented out code for readability

--- run.py
+++ run.py
@@ -1,206 +1,188 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-""" 
- Battleships game
-
-    The ability for the user to set the grid size
-    Warn the user if their guess is off-grid
-
-Must show: Programming constructs; repetition, selection, functions, composition, modules, aggregated data (arrays, lists, etc.)
-
-1. Implement a given algorithm as a computer program 
-2. Adapt and combine algorithms to solve a given problem
-3. Adequately use standard programming constructs: repetition, selection, functions, composition, modules, aggregated data (arrays, lists, etc.) 
-4. Explain what a given program does 
-5. Identify and repair coding errors in a program 
-6. Use library software for building a graphical user interface, or command-line interface, or web application, or mathematical software 
-7. Implement a data model, application features and business logic to manage, query and manipulate data to meet given needs in a particular real-world domain. 
-8. Demonstrate and document the development process through a version control system such as GitHub 
-9. Deploy a command-line application
-
-
-V1; 
-- A 15x15 grid; "~" for water. "#" for ship part. "X" for hit ship part. "o" for water that has been shot.
-- 5 ships; length 2, length 3, length 4 length 5. 
-- Randomise opponent placement
-- Randomise opponent guess
-- place ships only vertically or horizontally
-- Input for user to choose grid reference to 'shoot'
-- Score (ships remaining)
-
-
-v2:  To Do;
-- fix bug where user can shoot the same cell twice
-- add game instructions to menu screen
-- add option for user to define grid size
-- add different ship sizes, vertical & horizontal orientation only
-- reorder code so function definitions are at the top, and game logic is afterwards
-- after hitting a computer battleship, message to user should count how many battleships are remaining and inform the user
-
-extra validation
-- check if the user input is part of the board or not
-- check user input is not blank / just enter
-
-"""
 import random #load python module 'ramdom'
 import time #load python module 'time' which enables time delayed responses
 
-#Create two board names
-player_board = [['~']*10 for _ in range(10)] #create the players board, 10 wide with ~ for water
-computer_board_hidden = [['~']*10 for _ in range(10)] #create the hidden computer board, 10 wide with ~ for water
-computer_board_display = [['~']*10 for _ in range(10)] #the players view of the computers board
+#Create two visible boards to play on, and a hidden board to log the computers ship placements
+player_board = [['~']*10 for _ in range(10)]
+computer_board_hidden = [['~']*10 for _ in range(10)]
+computer_board_display = [['~']*10 for _ in range(10)]
 
 
 ### GAME MENU SCREEN SECTION ###
-def print_game_menu(): #creates a menu 'screen' before the board is printed. Placeholder for inputs to add later.
+def print_game_menu(): 
+    """
+    creates a menu 'screen' before the board is printed.
+    """
     print("Welcome to BATTLESHIPS!")
     print("Submit Y in the terminal to proceed to the game.")
 
 def menu_input():
-    user_input = input("Enter your selection: ") #prompts user to input their selection, as per the menu print instructions
-    if user_input.lower() == 'y': #converts user input to lower case, checks for match
+    """
+    prompts user to input their selection, as per the menu print instructions
+    converts user input to lower case, checks for match
+    """
+    user_input = input("Enter your selection: ") 
+    if user_input.lower() == 'y': 
         return True
     else:
         return False
 
 
 ### CREATE THE BOARD ###
-# Global variable to define the board itself. Now displaying two boards.  
 def print_board(board_title, game_board):
+    """
+    define the board itself. Now displaying two boards.
+    10 by 10, so 10 letters = A-J, with a blank at the start for row
+    print a line of dashes to outline the board
+    print ten lines of rows, and fill the board with waves ~
+    for single digit rows an extra space is needed to be printed for alignement
+
+    """
     print(board_title)
-    print ('   A B C D E F G H I J') #10 by 10, so 10 letters = A-J, with a blank at the start for row
-    print ('-----------------------') #top line of the board to seperate letters
-    board = [['~']*10 for _ in range(10)] #create the board with ~ wave symbol to represent water
-    
+    print ('   A B C D E F G H I J')
+    print ('-----------------------')
+    board = [['~']*10 for _ in range(10)]
+
     row_number = 1 #first row = 1
-    for row in game_board: 
+    for row in game_board:
         print(row_number, end=' ')
 
         if row_number < 10:
-            print(end=' ') #for single digit rows, an extra space is needed to align correctly
+            print(end=' ')
 
-        for column in row: #each cell in the row
-            print(column, end=' ') #print, but dont move onto the next row
-
-        print() #move to next row
-
-        row_number += 1 #increase the row number by 1
+        for column in row:
+            print(column, end=' ')
+        print() 
+        row_number += 1
     print()
 
 
 ### PLACE BATTLESHIPS SECTION ###
-#player ship placement
 def place_player_ships(board, ship_count):
-    for i in range(ship_count): #do this for as many ships are set to be placed
+    """
+    Player ship placement
+    Loop the logic until a proper board position is entered.
+    Input validation checks users input is;
+    - not blank
+    - is first a letter, then a number between 1-10
+    - is within the length and height of the board
+    Changes the cell in the player board to represent the placed ship using an "O"
+    to get the column of the board, convert letter to uppercase, ord function converts the unicode letter value to unicode integer
+    to get the row of the board, we take the int (integer) from user input, and subtract 1 (we count from 0)
+    """
+    for i in range(ship_count): 
         while True: #loop until a proper board position is entered, so no positions off the board
             print("Place ship number", i+1)
-
-            ship_position = input("Enter a ship position (For example: C5) and press Enter: ") # prompt user to input a ship position
-
-            #Input Validation: check users input is not blank / just an enter
+            ship_position = input("Enter a ship position (For example: C5) and press Enter: ")
             if not ship_position:
                 print("Oh no! You entered nothing! Try to type it again, just a letter and number, like c5: ")
                 continue
-
-            #Input Validation: check users input is a letter, then a digit, and the digit is between 1-10
             if not ship_position[0].isalpha() or not ship_position[1:].isdigit() or not 1 <= int(ship_position[1:]) <=10:
                 print("Oh no! Thats not a letter and number within the board! Try to type it again, just a letter and number, like c5: ")
                 continue
-
-            # to get the column of the board, convert letter to uppercase, ord function converts the unicode letter value to unicode integer
-            column = ord(ship_position[0].upper()) - ord('A') 
-
-            #to get the row of the board, we take the int (integer) from user input, and subtract 1 (we count from 0)
+            column = ord(ship_position[0].upper()) - ord('A')
             row = int(ship_position[1:]) - 1
-            
-            #input Validation: check the user input is within the length and height of the board
+
             if not (0 <= column < len(board) and 0 <= row < len(board)):
                 print("Oh no! Thats outside the board! Try to type it again, just a letter and number, like c5: ")
                 continue
-
-            #change the cell in the player board to represent the placed ship using an "O" 
             board[row][column] = 'O'
-
-            print("Column:", column, "Row:", row) #print to test these functions are working as intended
-            print_board("PLAYER BOARD", board) #print the updated player board with the ship placement
+            print("Column:", column, "Row:", row)
+            print_board("PLAYER BOARD", board)
             break
 
-#computer ship placement
+
 def place_computer_ships(board, ship_count):
+    """
+    Computers turn at ship placement
+    Check the 2d list of the board for the 'length' of rows in the board, generate a random integer within it
+    Check the first row of the board to count the number of columns, and generate a random integer within it
+    Check if the random board location is ~ (no ship placement)
+    Of it is ~, change it to 0 to 'place' the ship
+    #convert the column integer/letter
+    """
     for _ in range(ship_count):
         while True:
-            row = random.randint(0, len(board) -1) #check the 2d list of the board for the 'length' of rows in the board, generate a random integer within it
-            column = random.randint(0, len(board[0]) -1) #check the first row of the board to count the number of columns, and generate a random integer
-
-            if board[row][column] == '~': #check if the random board location is ~ (no ship placement)
-                board[row][column] = '0' #if it is ~, change it to 0 to 'place' the ship
-                print("Computer has placed: ", row+1, chr(column + ord('A'))) #convert the column integer/letter
+            row = random.randint(0, len(board) -1) 
+            column = random.randint(0, len(board[0]) -1)
+            if board[row][column] == '~':
+                board[row][column] = '0'
+                print("Computer has placed: ", row+1, chr(column + ord('A'))) 
             break
 
 ### GAME FUNCTIONS SECTION ###
-# Players Shot
 def player_shoot(computer_board_hidden, computer_board_display):
+    """
+    # Players Shot
+    prompt user for their move
+    Input validation checks users input is;
+    - not blank
+    - is first a letter, then a number between 1-10
+    - is within the length and height of the board
+    to get the column of the board, convert letter to uppercase.
+    ord function converts the unicode letter value to unicode integer
+    row of the board, take the int from user input, subtract 1 (we count from 0)
+    Check if the row has already been shot at, if it has then reprompt the user
+    Check if shot hits a ship on hidden board, change display board to hit 'X'
+    if the shot misses, change the display board wave ~ to M for 'Miss'
+    """
     while True:
-        shot_position = input("Your turn to fire at the enemy! Please enter a column and row (for example; C1): ") #prompt user for their move
+        shot_position = input("Your turn to fire at the enemy! Please enter a column and row (for example; C1): ")
 
-        #Input Validation: check users input is not blank / just an enter
         if not shot_position:
             print("Oh no! you entered nothing! Try to type it again, just a letter and number, like c5: ")
             continue 
 
-        #Input Validation: check users input is a letter, then a digit, and the digit is between 1-26
         if not shot_position[0].isalpha() or not shot_position[1:].isdigit() or not 1 <= int(shot_position[1:]) <=10:
             print("Oh no! That's not a letter and number within the board! Try to type it again, just a letter and number, like c5: ")
             continue
-
-        # to get the column of the board, convert letter to uppercase, ord function converts the unicode letter value to unicode integer
         column = ord(shot_position[0].upper()) - ord('A') 
 
-        #to get the row of the board, we take the int (integer) from user input, and subtract 1 (we count from 0)
+        
         row = int(shot_position[1:]) - 1
 
-        #input Validation: check the user input is within the length and height of the board
+    
         if not (0 <= column < len(computer_board_display) and 0 <= row < len(computer_board_display)):
             print("Oh no! Thats outside of the board! Try to type it again, just a letter and number, like c5: ")
             continue
-
-        #check if the row has already been shot at
         if computer_board_display[row][column] in ['M','X']:
             print("You've already shot at this position! Try again!")
             continue 
-        # check if shot hits a ship ('O') on hidden board
+
         if computer_board_hidden[row][column] == 'O':
-            # if it does, change display board to hit 'X'
+
             computer_board_display[row][column] = 'X'
             print("YOU HAVE HIT AN ENEMY BATTLESHIP!")
         else:
-            #if the shot misses, change the display board wave ~ to M for 'Miss' 
+
             computer_board_display[row][column] = 'M'
             print("YOU MISSED!")
         break      
 
 def computer_shoot(player_board):
+    """
+    User timer to give computer thinking time
+    select random position within the player board table for the computers go
+    checks board position has not already been 'guessed'
+    #has prints to test code is working, function needs to fix
+    #if the computer guess hits a ship
+    #or its a miss, change ~ to M on the player board
+    """
     print('Computer thinking about their shot...!')
-    time.sleep(3) #3 second delay
+    time.sleep(3)
 
     while True:
-        #select random position within the player board table for the computers go
         column = random.randint(0,9)
         row = random.randint(0,9)
 
-        #checks board position has not already been 'guessed' in previous computer goes
-        #has prints to test code is working, function needs to fix
         if player_board[row][column] not in ['X', 'M']:
-            if player_board[row][column] == 'O': #if the computer guess hits a ship
+            if player_board[row][column] == 'O': 
                 player_board[row][column] = 'X'
                 print("Computer has shot at position: " + chr(column + ord('A')) + str(row+1))
                 print("The enemy has hit your Battleship!")
                 print_board("PLAYER BOARD", player_board)
                 print_board("COMPUTER BOARD", computer_board_display)
                 break
-            #or its a miss, change ~ to M on the player board
+
             else:
                 player_board[row][column] = 'M'
                 print("The enemy has missed your ships!")
@@ -208,29 +190,37 @@ def computer_shoot(player_board):
                 print_board("COMPUTER BOARD", computer_board_display)
                 break
 
-#count the number of 'X' (direct) hit in board
 def count_hits(board):
+    """
+    #count the number of 'X' (direct) hit in board
+    """
     return sum(row.count('X') for row in board)
 
-#remove code from below; 
-def main():
 
-    while True: #While statement that checks if the menu input returns True, and if it does, breaks the menu loop, proceeds to the board
+def main():
+    """
+    While statement that checks if the menu input returns True.
+    if it does, breaks the menu loop, proceeds to the board
+    Print the boards for testing
+    place 3 ships on the player board
+    place 3 computer ships
+    Print the boards for testing
+    after players go, check computer boards for 3 X's, if so game over
+    after computer go, check player board for 3 X's, if so game over
+    """
+    while True:
         print_game_menu()
         if menu_input():
-            break 
+            break
 
-    #Print the boards for testing
     print_board("PLAYER BOARD", player_board)
     print_board("COMPUTER BOARD", computer_board_display)
 
-    place_player_ships(player_board, 3) #place 3 ships on the player board
+    place_player_ships(player_board, 3)
     print_board("PLAYER BOARD", player_board)
 
-    place_computer_ships(computer_board_hidden, 3) #place 3 computer ships
-    print_board("COMPUTER BOARD (hidden)", computer_board_hidden)
+    place_computer_ships(computer_board_hidden, 3)
 
-    #Print the boards for testing
     print_board("PLAYER BOARD", player_board)
     print_board("COMPUTER BOARD", computer_board_display)
 
@@ -243,13 +233,12 @@ def main():
     ### Main game loop ###
     while True:
         player_shoot(computer_board_hidden, computer_board_display)
-        #after players go, check computer boards for 3 X's, if so game over
+
         if count_hits(computer_board_display) == 3:
             print("VICTORY! You sank all enemy Battleships!")
             print("GAME OVER - you are the winner!")
             break
 
-        #after computer go, check player board for 3 X's, if so game over
         computer_shoot(player_board)
         if count_hits(player_board) == 3:
             print("YOU SUFFER DEFEAT AT THE HANDS OF THE ENEMY!")
@@ -258,4 +247,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()    
+    main()
